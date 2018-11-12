739c96985565ad00d8f563730843274facd358e3
autodocs <autodocs>
2018-11-12 16:17:46 +0000

    build based on 86d669f

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,15 @@ var documenterSearchIndex = {"docs": [
     "page": "AltDistributions",
     "title": "AltDistributions.AltMvNormal",
     "category": "type",
-    "text": "AltMvNormal(?, μ, L)\n\n\nInner constructor used internally, for specifying L directly when the first argument is Val{:L}.\n\nYou don\'t want to use this unless you obtain L directly. Use a Cholesky factorization instead.\n\n\n\n\n\nAltMvNormal(μ, Σ)\n\n\nMultivariate normal distribution with mean μ and covariance matrix Σ, which can be an abstract matrix (eg a factorization) or I. If Σ is not symetric because of numerical error, wrap in LinearAlgebra.Symmetric.\n\nUse the AltMvNormal(Val(:L), μ, L) constructor for using LL\'=Σ directly.\n\n\n\n\n\n"
+    "text": "AltMvNormal(?, μ, L)\n\n\nInner constructor used internally, for specifying L directly when the first argument is Val{:L}.\n\nYou don\'t want to use this unless you obtain L directly. Use a Cholesky factorization instead.\n\n\n\n\n\nAltMvNormal(μ, Σ)\n\n\nMultivariate normal distribution with mean μ and covariance matrix Σ, which can be an abstract matrix (eg a factorization) or I. If Σ is not symetric because of numerical error, wrap in LinearAlgebra.Symmetric.\n\nUse the AltMvNormal(Val(:L), μ, L) constructor for using LL\'=Σ directly.\n\nAlso, see StdCorrFactor for formulating L from standard deviations and a Cholesky factor of a correlation matrix:\n\nAltMvNormal(μ, StdCorrFactor(σ, S))\n\n\n\n\n\n"
+},
+
+{
+    "location": "#AltDistributions.StdCorrFactor",
+    "page": "AltDistributions",
+    "title": "AltDistributions.StdCorrFactor",
+    "category": "type",
+    "text": "StdCorrFactor(σ, F)\n\n\nA factor L of a covariance matrix Σ = LL\' given as L = Diagonal(σ) * F. Can be used in place of L, without performing the multiplication.\n\n\n\n\n\n"
 },
 
 {
@@ -29,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "AltDistributions",
     "title": "AltDistributions",
     "category": "section",
-    "text": "AltMvNormal\nLKJL"
+    "text": "AltMvNormal\nStdCorrFactor\nLKJL"
 },
 
 ]}
